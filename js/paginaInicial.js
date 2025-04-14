486c1246b0c3f9c9586d4bca16ac500ec2c3d04b
MarcoTulioDev21 <tuliomarcos1440@gmail.com>
2025-04-13 22:44:44 -0300

    Delete js directory

--- js/paginaInicial.js
+++
@@ -1,6 +0,0 @@
-function redirecionarParaLogin(){
-    window.location.href = "login.html";  // redirecionar para uma página nossa
-}
-function redirecionaParaCadastro(){
-    window.location.href = "cadastro.html";  // redirecionar para uma página nossa
-}
