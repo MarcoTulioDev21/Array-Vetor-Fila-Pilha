e4b49de5fd7110e112c6ed41925105002778aa75
MarcoTulioDev21 <tuliomarcos1440@gmail.com>
2025-04-13 22:44:06 -0300

    Delete api directory

--- api/get_user_id.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['user_id'])) {
-    echo $_SESSION['user_id'];
-} else {
-    echo "Não foi possível localizar seu ID. \nVocê acessou com o Google? Leia: Termos e Condições \nAcessou normal? Saia e faça o login novamente.";
-}
-?>
